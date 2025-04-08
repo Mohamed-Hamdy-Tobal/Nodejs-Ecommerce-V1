c8d8cd548c9d6c33c4f63ca0f02161514366f2c9
Mohamed-Hamdy-Tobal <hamdy3tobal@gmail.com>
2025-04-08 12:55:49 +0200

    Add MONGODB_URI To Config

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const config = {
-  dbUrl: process.env.MONGO_URL,
+  dbUrl: process.env.MONGODB_URI,
   port: process.env.PORT || 4000,
-  mode: process.envNODE_EVN || "development",
+  mode: process.env.NODE_EVN || "development",
 };
