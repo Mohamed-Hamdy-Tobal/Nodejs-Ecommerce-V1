a052a7a06d7228ccd9dfa09154cf1be23af8bce4
Mohamed-Hamdy-Tobal <hamdy3tobal@gmail.com>
2025-04-20 11:00:53 +0200

    feat: add category router for CRUD operations on categories

--- routes/category.route.js
+++ routes/category.route.js
@@ -1,13 +1,20 @@
 import express from "express";
 import {
   createCategory,
+  deleteCategory,
   getAllCategories,
   getSingleCategory,
+  updateCategory,
 } from "../controllers/category.controller.js";
 
 const router = express.Router();
 
 router.route("/").get(getAllCategories).post(createCategory);
-router.route("/:id").get(getSingleCategory);
+
+router
+  .route("/:id")
+  .get(getSingleCategory)
+  .put(updateCategory)
+  .delete(deleteCategory);
 
 export const categoryRouter = router;
