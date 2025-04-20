a10e5ad034000b6f303ff7ca2922e3b4449644dd
Mohamed-Hamdy-Tobal <hamdy3tobal@gmail.com>
2025-04-20 10:54:18 +0200

    Category Model and Schema

--- models/category.model.js
+++ models/category.model.js
@@ -1,11 +1,22 @@
 import mongoose from "mongoose";
 
-const categorySchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
+const categorySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Category name is required"],
+      unique: [true, "Category must be unique"],
+      minlength: [3, "Too short name"],
+      maxlength: [32, "Too long name"],
+    },
+    slug: {
+      type: String,
+      lowercase: true,
+    },
+    image: String,
   },
-});
+  { timestamps: true } // created_at, updated_at
+);
 
 const CategoryModel = mongoose.model("Category", categorySchema);
 
