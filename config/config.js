31bc43bbb113ee28147f3529d523162fde0aa466
Mohamed-Hamdy-Tobal <hamdy3tobal@gmail.com>
2025-04-23 10:03:15 +0200

    Update in Config Settings

--- config/config.js
+++ config/config.js
@@ -5,5 +5,5 @@ dotenv.config();
 export const config = {
   dbUrl: process.env.MONGODB_URI,
   port: process.env.PORT || 4000,
-  mode: process.env.NODE_EVN || "development",
+  mode: process.env.NODE_ENV || "development",
 };
