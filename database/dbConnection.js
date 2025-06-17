66c2fde5e7c4d8ac0facbf99676ded92267d3101
Mohamed-Hamdy-Tobal <hamdy3tobal@gmail.com>
2025-06-17 11:09:47 +0300

    Enhance in DB Connection

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -1,38 +1,40 @@
-import mongoose from 'mongoose';
-import { config } from '../config/config.js';
+import mongoose from "mongoose";
+import { config } from "../config/config.js";
+import dotenv from "dotenv";
 
+dotenv.config();
 // Options for mongoose connection
 const mongooseOptions = {
   autoIndex: true, // Build indexes
   maxPoolSize: 10, // Maintain up to 10 socket connections
   serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
   socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
-  family: 4 // Use IPv4, skip trying IPv6
+  family: 4, // Use IPv4, skip trying IPv6
 };
 
 // Connect to MongoDB
 export const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(config.dbUrl, mongooseOptions);
-    
+    const conn = await mongoose.connect(config.dbUrl || process.env.MONGODB_URI, mongooseOptions);
+
     console.log(`MongoDB Connected: ${conn.connection.host}`);
-    
+
     // Handle connection events
-    mongoose.connection.on('error', (err) => {
+    mongoose.connection.on("error", (err) => {
       console.error(`MongoDB connection error: ${err}`);
     });
-    
-    mongoose.connection.on('disconnected', () => {
-      console.warn('MongoDB disconnected');
+
+    mongoose.connection.on("disconnected", () => {
+      console.warn("MongoDB disconnected");
     });
-    
+
     // If Node process ends, close the MongoDB connection
-    process.on('SIGINT', async () => {
+    process.on("SIGINT", async () => {
       await mongoose.connection.close();
-      console.log('MongoDB connection closed due to app termination');
+      console.log("MongoDB connection closed due to app termination");
       process.exit(0);
     });
-    
+
     return conn;
   } catch (error) {
     console.error(`Error connecting to MongoDB: ${error.message}`);
