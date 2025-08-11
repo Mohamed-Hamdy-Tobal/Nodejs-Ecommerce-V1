e909882a210e5ea58f5762c35c244280510e814f
Mohamed Tobal <mohamedtobal369@gmail.com>
2025-08-11 20:26:27 +0300

    Implement Generate Tokens Util Function

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -1,8 +1,20 @@
 import jwt from "jsonwebtoken";
 import { config } from "../config/config.js";
 
-export const generateToken = (userId) => {
+export const generateTokens = (userId) => {
+  const accessToken = jwt.sign({ id: userId }, config.jwt.secret, {
+    expiresIn: config.jwt.accessExpiresIn || "30m", // Default to 30 minutes if not set
+  });
+
+  const refreshToken = jwt.sign({ id: userId }, config.jwt.refreshSecret, {
+    expiresIn: config.jwt.refreshExpiresIn || "7d", // Default to 7 days if not set
+  });
+
+  return { accessToken, refreshToken };
+};
+
+export const generateAccessToken = (userId) => {
   return jwt.sign({ id: userId }, config.jwt.secret, {
-    expiresIn: config.jwt.expiresIn,
+    expiresIn: config.jwt.accessExpiresIn,
   });
 };
