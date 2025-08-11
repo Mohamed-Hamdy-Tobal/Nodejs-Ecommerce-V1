ae4ed9ac91bc67b0b5e40802f9f947911471b67d
Mohamed Tobal <mohamedtobal369@gmail.com>
2025-08-11 20:25:09 +0300

    Implement Login and Refresh Token Routes

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,11 +1,11 @@
 import express from "express";
-import { registerValidation } from "../validation/authValidation.js";
-import { register } from "../controllers/auth.controller.js";
-
+import { loginValidation, registerValidation } from "../validation/authValidation.js";
+import { login, refreshToken, register } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
 router.post("/register", registerValidation, register);
-
+router.post("/login", loginValidation, login);
+router.post("/refresh-token", refreshToken);
 
 export const authRouter = router;
