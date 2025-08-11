e0128cf2d42cdb40401ec2baec9cfdc5b051bc07
Mohamed Tobal <mohamedtobal369@gmail.com>
2025-08-11 15:27:59 +0300

    add jwt config

--- config/config.js
+++ config/config.js
@@ -6,4 +6,9 @@ export const config = {
   dbUrl: process.env.MONGODB_URI,
   port: process.env.PORT || 4000,
   mode: process.env.NODE_ENV || "development",
+
+  jwt: {
+    secret: process.env.JWT_SECRET,
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  },
 };
